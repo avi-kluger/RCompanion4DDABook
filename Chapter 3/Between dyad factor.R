3c313e2842c2e710d0f3b4eea29932c9fd09df8b
Avi Kluger <avik@savion.huji.ac.il>
2018-09-27 16:30:37 +0300

    Add Chapter 3 Table 3.1 and 3.3

---
+++ Chapter 3/Between dyad factor.R
@@ -0,0 +1,53 @@
+################################################################################
+#    **************************** R companion for ************************** 
+#
+# Kenny, D. A., Kashy, D. A., & Cook, W. L. (2006). Dyadic data analysis. 
+# New York: Guilford Press.
+#
+# written by Avi Kluger: avik@savion.huji.ac.il
+#
+#                              CHAPTER 3 -- Table 3.1
+###############################################################################
+rm(list = ls())                               # Clean the Global Environment
+cat ("\014")                                  # Clean the R console
+if (is.null(dev.list()) == FALSE) dev.off()   # Clean Plots
+
+# Read Klump et al sample data (in SPSS format) from Kenny's book site
+
+if (!require('foreign')) install.packages('foreign'); library('foreign')
+Klump_et_al <- read.spss("http://davidakenny.net/kkc/c3/klumpindividual.sav", 
+                         to.data.frame=TRUE)
+
+if (!require("nlme")) install.packages("nlme"); library(nlme)
+
+# Null model
+
+mlm     <- gls(total ~ 1, data = Klump_et_al,
+           na.action = "na.omit", verbose = TRUE,
+           correlation=corCompSymm(form = ~1|dyad))
+icc     <- intervals(mlm) ["corStruct"]
+round(as.data.frame(icc), 3)
+
+# Between dyad factor (cohort) model
+# Make cohort a dummy code
+Klump_et_al$cohort <- (Klump_et_al$cohort - 11) / 6
+
+if (!require('tidyverse')) install.packages('tidyverse'); library('tidyverse')
+group_by(Klump_et_al, cohort) %>% summarize(m = mean(total),
+                                            sd = sd(total))
+
+mlm     <- gls(total ~ cohort, data = Klump_et_al,
+           na.action = "na.omit", verbose = TRUE,
+           correlation=corCompSymm(form = ~1|dyad))
+fit <- (summary(mlm))
+icc     <- as.data.frame(intervals(mlm) ["corStruct"])
+round(icc, 3)
+
+# Calculate d with formulae from p. 57
+t      <- as.numeric(fit$tTable["cohort", "t-value"])
+ri     <- as.numeric(icc["corStruct.est."])
+nDyads <- sqrt(nrow(Klump_et_al)/2)
+dD     <- 2*t / nDyads
+d      <- dD * sqrt((1+ ri)/ 2)
+round(dD, 3)
+round(d, 3)
---
+++ Chapter 3/Within dyad factor.R
@@ -0,0 +1,36 @@
+################################################################################
+#    **************************** R companion for ************************** 
+#
+# Kenny, D. A., Kashy, D. A., & Cook, W. L. (2006). Dyadic data analysis. 
+# New York: Guilford Press.
+#
+# written by Avi Kluger: avik@savion.huji.ac.il
+#
+#                              CHAPTER 3 -- Table 3.3
+###############################################################################
+rm(list = ls())                               # Clean the Global Environment
+cat ("\014")                                  # Clean the R console
+if (is.null(dev.list()) == FALSE) dev.off()   # Clean Plots
+
+# Example data DIFFERENT FROM THE BOOK based on Lehmann study of humility
+
+library(readr)
+Humility <- read_csv("Chapter 3/Lehmann Humility experiement.csv") [, -1]
+
+if (!require('tidyverse')) install.packages('tidyverse'); library('tidyverse')
+group_by(Humility, Condition) %>% summarize(m.l = mean(Humility.listener),
+                                            sd.l = sd(Humility.listener),
+                                            m.s = mean(Humility.Speaker),
+                                            sd.s = sd(Humility.Speaker))
+
+# Distraction condition only
+Humility <- Humility[which(Humility$Condition == 1), ]
+
+# ICC based on Pearson's r
+
+round(cor(Humility [, c("Humility.listener", "Humility.Speaker")]), 2)
+
+t.test(Humility$Humility.listener, 
+       Humility$Humility.Speaker, 
+       paired=TRUE, 
+       conf.level=0.95)
