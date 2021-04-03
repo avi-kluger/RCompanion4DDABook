f38c97b88bc136ef5a4c76fc39be781730212923
avik@savion.huji.ac.il <avik@savion.huji.ac.il>
2021-04-03 14:23:13 +0300

    Update for Chapter 2

--- Chapter 2/Calculate ICC w MLM.R
+++ Chapter 2/Calculate ICC w MLM.R
@@ -4,7 +4,7 @@
 # Kenny, D. A., Kashy, D. A., & Cook, W. L. (2006). Dyadic data analysis. 
 # New York: Guilford Press.
 #
-# written by Avi Kluger: avik@savion.huji.ac.il
+# written by Avi Kluger: avik@savion.huji.ac.il, April 2021
 #
 #                              CHAPTER 2 -- Table 2.1
 ################################################################################
@@ -12,7 +12,7 @@ rm(list = ls())                               # Clean the Global Environment
 cat ("\014")                                  # Clean the R console
 if (is.null(dev.list()) == FALSE) dev.off()   # Clean Plots
 
-dyad_df <- read.csv(text = "dyad, x, xprime
+w <- read.csv(text = "dyad, x, xprime
 1,8,6
 2,5,3
 3,7,2
@@ -26,17 +26,28 @@ dyad_df <- read.csv(text = "dyad, x, xprime
 
 if (!require('tidyverse')) install.packages('tidyverse'); library('tidyverse')
 
-# Transform dyad df into individual df, and keep only the dyad and score
+# Correlate x and x'; meaningless if dyads are indistinguishable
+w %>% dplyr::select(-dyad) %>% cor %>% round(2) 
 
-individual_df <- dyad_df %>% gather(dyadMember, score, x:xprime) %>% 
-                 select(-dyadMember)
+# Calculate ICC on wide data with ICC function from Psych 
+library("psych")
+w %>% dplyr::select(-dyad) %>% ICC
 
-# Calculate intra class correlation (ICC) with MLM
+# A two-tailed CI, but see the Note in the help of the psych:ICC function 
+w %>% dplyr::select(-dyad) %>% ICC(alpha = .025)
+
+# Transform dyad data into individual data &  keep the dyad and score variables
+l <- w %>%
+  pivot_longer(                           
+    cols = !dyad
+)
+l 
+
+# Calculate intraclass correlation (ICC) with MLM
 # Code for ICC is based on http://davidakenny.net/papers/k&h/MLM_R.pdf
 
 if (!require("nlme")) install.packages("nlme"); library(nlme)
-mlm     <- gls(score   ~ 1, correlation = corCompSymm(form = ~1|dyad),
-               data = individual_df)
+mlm     <- gls(value ~ 1, correlation = corCompSymm(form = ~1|dyad), data = l)
 icc     <- intervals(mlm) ["corStruct"]
 icc     <- round(as.data.frame(icc), 2)
 cat(paste0("\nICC = ", icc[2], " [", icc[1], ", ", icc[3], "]"))
