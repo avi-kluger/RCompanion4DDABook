f94a29391609526e8803cd600f83210784ddd945
unknown <avik@savion.huji.ac.il>
2021-04-16 14:33:00 +0300

    Stream line chapter 4 example

--- Chapter 4/ReadDataTable4.3.R
+++ Chapter 4/ReadDataTable4.3.R
@@ -4,7 +4,7 @@
 # Kenny, D. A., Kashy, D. A., & Cook, W. L. (2006). Dyadic data analysis. 
 # New York: Guilford Press.
 #
-# written by Avi Kluger: avik@savion.huji.ac.il
+# written by Avi Kluger: avik@savion.huji.ac.il April 2021
 #
 #                              CHAPTER 4 -- Table 4.3
 ###############################################################################
@@ -13,9 +13,8 @@ cat ("\014")                                  # Clean the R console
 if (is.null(dev.list()) == FALSE) dev.off()   # Clean Plots
 
 # Read Klump et al sample data (in SPSS format) from Kenny's book site
-if (!require('foreign')) install.packages('foreign'); library('foreign')
-couples_df <- read.spss("http://davidakenny.net/kkc/c4/table4.3.sav", 
-                         to.data.frame=TRUE)
+if (!require('tidyverse')) install.packages('tidyverse'); library('tidyverse')
+couples_df <- haven::read_sav("http://davidakenny.net/kkc/c4/table4.3.sav")
 
 if (!require("nlme")) install.packages("nlme"); library(nlme)
 
@@ -25,19 +24,19 @@ mlm_null     <- gls(future ~ 1, correlation=corCompSymm(form = ~1|dyad),
 icc     <- as.data.frame(intervals(mlm_null) ["corStruct"])
 round(icc, 3)
 
-mlm_full     <- gls(future ~ contrib*culture, 
-                    correlation=corCompSymm(form = ~1|dyad),
-                    data = couples_df)
-(fit <- summary(mlm_full))
+# Obtain the results of p.91
+mlm_full <-update(mlm_null, . ~ . + contrib*culture)
+summary(mlm_full)
 
 getVarCov(mlm_full)
+
+# Obtain the result of p.94
 intervals(mlm_full)
 icc     <- as.data.frame(intervals(mlm_full) ["corStruct"])
 round(icc, 3)
 
-
+# Obtain results of p. 91 and 94 (with no direct calculation of ICC with lmer)
 if (!require("lme4")) install.packages("lme4"); library(lme4)
-
 mlm_full <- lmer(future ~ contrib*culture + (1 | dyad), data = couples_df)
 summary(mlm_full)
 
